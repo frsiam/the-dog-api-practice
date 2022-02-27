326c3a6a4d635e8acfcb1d3c44b4fe5b3d247c97
frsiam <siam.frs@gmail.com>
2022-02-27 19:28:47 +0600

    add some bootstrap class

--- js/app.js
+++ js/app.js
@@ -5,17 +5,18 @@ const loadDog = () => {
         .then(data =>displayDog(data))
 }
 
-const displayDog = allDogs => {
+const displayDog = dogs => {
     // console.log(allDogs);
+    const dogCard = document.getElementById('dog-card');
+    const allDogs = dogs.slice(0,12)
     for(const dog of allDogs){
-        // console.log(dog);
-        const dogCard = document.getElementById('dog-card');
+        console.log(dog);
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="${dog.image.url}" class="card-img-top" height='250px' alt="...">
-            <div class="card-body">
+        <div class="card h-100 border-info bg-dark">
+            <img src="${dog.image.url}" class="p-3 card-img-top bg-danger" height='250px' alt="...">
+            <div class="card-body bg-dark text-white mb-2 mx-2">
                 <h5 class="card-title">${dog.name}</h5>
                 <p class="card-text">${dog.temperament}</p>
             </div>
@@ -41,20 +42,29 @@ const getTheUrl = dogId => {
 const showDetails = singleDog => {
     console.log(singleDog)
     const detailsDiv = document.getElementById('staticBackdrop')
+    document.getElementById('staticBackdrop').textContent = ''
     const newDiv = document.createElement('div')
     newDiv.classList.add('modal-dialog')
     newDiv.innerHTML = `
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <h3 class="modal-title" id="staticBackdropLabel">Details about "${singleDog.name}"</h3>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
-            <p>This is some placeholder content to show the scrolling behavior for modals. We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. When content becomes longer than the prefedined max-height of modal, content will be cropped and scrollable within the modal.</p>
+        <ul>
+            <li><b class="text-primary">Type : </b>${singleDog.bred_for}</li>
+            <li><b class="text-primary">Origin : </b>${singleDog.origin}</li>
+            <li><b class="text-primary">Life Span : </b>${singleDog.life_span}</li>
+            <li><b class="text-primary">Origin : </b>${singleDog.origin}</li>
+            <li><b class="text-primary">Breed Group : </b>${singleDog.breed_group}</li>
+            <li><b class="text-primary">Height : </b><strong>${singleDog.height.imperial}</strong></li>
+            <li><b class="text-primary">Weight : </b>${singleDog.weight.imperial}</li>
+            <li><b class="text-primary">Description : </b>${singleDog.temperament}</li>
+        </ul>
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Understood</button>
         </div>
     </div>
     `
