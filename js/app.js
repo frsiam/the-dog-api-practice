f9e84b0a848be176d532c0e3fc6aa6c4ca244169
frsiam <siam.frs@gmail.com>
2022-02-27 17:13:54 +0600

    creat a div and append modal for showing the details of the dog

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ const loadDog = () => {
 }
 
 const displayDog = allDogs => {
-    console.log(allDogs);
+    // console.log(allDogs);
     for(const dog of allDogs){
         // console.log(dog);
         const dogCard = document.getElementById('dog-card');
@@ -20,7 +20,9 @@ const displayDog = allDogs => {
                 <p class="card-text">${dog.temperament}</p>
             </div>
             <div class='text-center mb-3'>
-                <button onclick="showDetails(${dog.id})" class='btn btn-primary py-2 px-5'>See Details</button>
+                <button onclick="getTheUrl(${dog.id})" type="button" class="btn btn-primary py-2 px-5"   data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                See Details
+                </button>
             </div>
         </div>
         `
@@ -28,10 +30,33 @@ const displayDog = allDogs => {
     }
 }
 
-const showDetails = dogId => {
+const getTheUrl = dogId => {
     console.log(dogId)
     const url = `https://api.thedogapi.com/v1/breeds/${dogId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => showDetails(data))
+}
+
+const showDetails = singleDog => {
+    console.log(singleDog)
+    const detailsDiv = document.getElementById('staticBackdrop')
+    const newDiv = document.createElement('div')
+    newDiv.classList.add('modal-dialog')
+    newDiv.innerHTML = `
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <p>This is some placeholder content to show the scrolling behavior for modals. We use repeated line breaks to demonstrate how content can exceed minimum inner height, thereby showing inner scrolling. When content becomes longer than the prefedined max-height of modal, content will be cropped and scrollable within the modal.</p>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+    </div>
+    `
+    detailsDiv.appendChild(newDiv)
 }
