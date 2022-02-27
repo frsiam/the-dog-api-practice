92d75209630b1b7dd6b167b4255112a8824f1f07
frsiam <siam.frs@gmail.com>
2022-02-27 16:22:42 +0600

    create a function for show the details about the dog which dog button is cliked

--- js/app.js
+++ js/app.js
@@ -8,19 +8,30 @@ const loadDog = () => {
 const displayDog = allDogs => {
     console.log(allDogs);
     for(const dog of allDogs){
-        console.log(dog.name);
+        // console.log(dog);
         const dogCard = document.getElementById('dog-card');
         const div = document.createElement('div')
         div.classList.add('col')
         div.innerHTML = `
         <div class="card h-100">
-            <img src="${dog.image.url}" class="card-img-top" alt="...">
+            <img src="${dog.image.url}" class="card-img-top" height='250px' alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${dog.name}</h5>
+                <p class="card-text">${dog.temperament}</p>
+            </div>
+            <div class='text-center mb-3'>
+                <button onclick="showDetails(${dog.id})" class='btn btn-primary py-2 px-5'>See Details</button>
             </div>
         </div>
         `
         dogCard.appendChild(div)
     }
+}
+
+const showDetails = dogId => {
+    console.log(dogId)
+    const url = `https://api.thedogapi.com/v1/breeds/${dogId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 }
